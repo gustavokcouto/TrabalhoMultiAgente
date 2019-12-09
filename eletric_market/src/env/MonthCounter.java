a9d58580595277d9f86f8840d076cb5fb1bcb0a9
Gustavo Claudio Karl Couto <gustavokarlcouto@gmail.com>
2019-12-08 23:44:30 -0300

    Add month counter artifact

---
+++ eletric_market/src/env/MonthCounter.java
@@ -0,0 +1,19 @@
+package electric_market;
+
+import jason.asSyntax.Atom;
+import cartago.*;
+
+public class MonthCounter extends Artifact {
+    final static long TICK_TIME = 5000;
+    void init(){
+        defineObsProperty("month",0);
+        execInternalOp("count");
+    }
+    @INTERNAL_OPERATION void count(){
+        while(true){
+            ObsProperty prop  = getObsProperty("month");
+            prop.updateValue(prop.intValue()+1);
+            await_time(TICK_TIME);
+        }
+    }
+}
